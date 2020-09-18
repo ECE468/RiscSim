0ae2886cf492c5830362d95a47dc262259438a64
Milind Kulkarni <milind@purdue.edu>
2020-09-17 21:50:34 -0400

    add support for number of regs

--- driver.py
+++ driver.py
@@ -8,6 +8,16 @@ import machine
 import sys
 
 if __name__ == '__main__' :
+
+    if len(sys.argv) > 2 :
+        if (int(sys.argv[2]) < 32) :
+            print("Cannot initialize simulator with fewer than 32 registers")
+            quit()
+        print("Initializing machine with " + sys.argv[2] + " registers")
+        config.machine = machine.Machine(numIntRegisters = int(sys.argv[2]), numFloatRegisters = int(sys.argv[2]), timingModel = timingmodel.basicTimingModel)
+    else :
+        print("Using default machine configuration with 32 registers")
+            
     p = program.Program()
     p.buildCodeFromFile(sys.argv[1])
     
